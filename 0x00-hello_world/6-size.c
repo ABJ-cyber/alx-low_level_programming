8bd2c0a1a0ec8a1a4ce924240d98dc8b7901cbcb
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-06-29 04:54:49 -0700

    Added size not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int integerType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long int longIntType;
+	long long int longLongIntType;
+
+	printf("Size of a char: %ld\n", sizeof(charType);
+	printf("Size of an int: %ld\n", sizeof(integerType);
+	printf("size of a long int: %ld\n", sizeof(longIntType);
+	printf("size of a long long int: %ld\n", sizeof(longLongIntType);
+	printf("size of a float: %ld\n", sizeof(floatType);
+	return (0);
