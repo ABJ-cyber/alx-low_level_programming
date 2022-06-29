b3f04d601f10dc583ff8f9dc63b605166202166d
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-06-29 08:55:48 -0700

    Fixed 6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long int longIntType;
 	long long int longLongIntType;
 
-	printf("Size of a char: %ld\n byte(s)", sizeof(charType));
-	printf("Size of an int: %ld\n byte(s)", sizeof(integerType));
-	printf("size of a long int: %ld\n byte(s)", sizeof(longIntType));
-	printf("size of a long long int: %ld\n byte(s)", sizeof(longLongIntType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+	printf("size of a long int: %ld byte(s)\n", sizeof(longIntType));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(longLongIntType));
 	printf("size of a float: %ld\n byte(s)", sizeof(floatType));
 
 	return (0);
