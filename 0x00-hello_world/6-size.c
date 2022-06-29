406c5c2ee18dd765b36ecb3d45ed7ed8f1ad4cae
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-06-29 08:57:00 -0700

    Fixed 6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,7 +18,7 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
 	printf("size of a long int: %ld byte(s)\n", sizeof(longIntType));
 	printf("size of a long long int: %ld byte(s)\n", sizeof(longLongIntType));
-	printf("size of a float: %ld\n byte(s)", sizeof(floatType));
+	printf("size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
