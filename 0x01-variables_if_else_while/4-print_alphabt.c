d3d0ff655c628a3fc8aa3f681c5fff21b5eaf19e
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-01 10:21:07 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e')
+		{
+			if (a != 'q')
+				putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
