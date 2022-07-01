e389d82d0803e622e0cbae48cf8d685c86ee05bd
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-01 01:09:41 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	return (0);
+}
