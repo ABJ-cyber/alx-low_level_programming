de67802fede4ded55a45b153578b014ca1199f30
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-01 12:39:50 -0700

    as

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '1'; a <= '9'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+			putchar(a);
+			putchar(b);
+			putchar(',');
+	}
+}
