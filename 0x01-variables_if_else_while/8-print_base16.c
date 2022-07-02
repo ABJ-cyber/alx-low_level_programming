0480ead4bb500b46cb304b552c15bab5f244c529
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-02 07:34:00 -0700

    8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,8 +3,19 @@
 /**
  * main - Entry point
  *
- * return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
+	int a;
+	int b;
 
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
