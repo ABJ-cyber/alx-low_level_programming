01da51fcd7282ceda1ca368555f557f2fa3f3d07
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-02 07:05:50 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,13 @@
 int main(void)
 {
 	int a;
-	int b;
 
-	for (a = '1'; a <= '9'; a++)
+	for (a = '1'; a <= '8'; a++)
 	{
-		for (b = '1'; b <= '9'; b++)
-		{
-			putchar(a);
-			putchar(b);
-			putchar(',');
-		}
+		putchar(a);
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('9');
 	return (0);
 }
