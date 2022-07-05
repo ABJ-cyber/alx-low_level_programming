5002a76d19121da613a562d38e3909ad0f85511a
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-05 12:44:20 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value
+ * @n: value to find absolute
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	int x;
+
+	x = n * n;
+	x = n/n;
+
+	return (x);
+}
