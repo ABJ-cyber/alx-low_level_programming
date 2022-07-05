dcf6725304f9943b1fa4c45797bd7e42992fb525
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-05 14:09:39 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints to 98
+ * @n: starting point
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i);
+		if (i < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
