ecbdfa9141ede91d9aef0c3209d902862a4a1d01
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-05 10:49:06 -0700

    print alpha x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - Entry point
+ *
+ * Return: void
+ */
+int print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n');
+	}
+	return 
+}
