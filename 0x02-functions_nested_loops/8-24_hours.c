de21aac47bdd9d2675c08701a291a10b390f36ff
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-05 13:33:18 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 5; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				d = (a - (a % 10))/10;
+				if (d <= 0)
+					d = 0;
+				e = a % 10;
+
+				_putchar(d + '0');
+				_putchar(e + '0');
+				_putchar(':');
+				_putchar(b + '0');
+				_putchar(c + '0');
+			}
+		}
+	}
+
+}	
