83a2bf13ed05429ff78d5f161e07c76c8c2f8618
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-08 02:22:48 -0700

    fiz buz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry poinnt
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		else if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
