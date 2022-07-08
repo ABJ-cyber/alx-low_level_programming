399c757abb1c3de46cf04e8139c95f9f107a6e9b
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-08 00:36:43 -0700

    as

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - prints numbers from 0 to 9
@@ -11,7 +12,7 @@ void print_numbers(void)
 
 	for (i = 1; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		putchar(i + '0');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
