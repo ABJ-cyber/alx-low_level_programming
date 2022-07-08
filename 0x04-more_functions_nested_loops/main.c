5697a242560f5e4880f80ebc5010202b8c382d46
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-08 02:55:00 -0700

    1 pedantic

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,13 +9,9 @@ int main(void)
 {
 	int i;
 
-	i = 9;
+	i = '9';
 
-	print_triangle(2);
-	print_triangle(10);
-	print_triangle(1);
-	print_triangle(0);
-	print_triangle(-2);
+	_isdigit(i);
 
 	return (0);
 }
