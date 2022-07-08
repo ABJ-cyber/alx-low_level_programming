18c9d27d92cf125a76359ca4dec093d1bbb5b738
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-08 01:18:05 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints line
+ * @n: number oftimes
+ * Return: void
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+		putchar('_');
+	putchar('\n');
+}
