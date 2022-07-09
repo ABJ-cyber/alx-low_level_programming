31ec0b1b692c374fe15f0389589cbc69cf317510
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-09 01:26:47 -0700

    numbers for life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * void print_number - prints a number
+ * @n: number to print
+ * Return: null
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+		putchar('0');
+
+	if (n / 10)
+		printnumber(n / 10);
+
+	putchar((n % 10) + '0');
+}
