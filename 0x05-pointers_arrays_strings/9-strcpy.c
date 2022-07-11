1d887c65f00d9ab31d3644bf86e3d02e32a387a4
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 07:11:44 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - Copies a string
+ * @dest: destination
+ * @src: sournce
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	dest = src;
+
+	return (*dest);
+}
