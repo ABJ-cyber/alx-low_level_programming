3be42576d5b776ce973be31093303f56da27f063
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 05:05:49 -0700

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to reverse
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int temp, i, left, len, right;
+
+	left = 0;
+	len = strlen(s);
+	right = len - 1;
+
+	for (i = left; i < right; i++)
+	{
+		temp = s[i];
+		s = s[right];
+		s[right] = temp;
+		right--;
+	}
+}
