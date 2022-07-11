1e4eb3332b0df75742cac465c4a4c3e37198a832
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 06:02:30 -0700

    print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints last half of a string
+ * @str: input string
+ * Return: void
+ */
+
+void puts_half(char *str);
+{
+	int len, i;
+
+	len = strlen(str);
+	if (len % 2 == 0)
+		i = len /2;
+	else
+		i = (len - 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
