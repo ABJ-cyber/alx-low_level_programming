2eaddbec5a30591cd0dbab203854631a2e71ea3e
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 06:39:33 -0700

    print n elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - prints n elements in an array
+ * @a: array
+ * @n: number of elements to print
+ * Return: void
+ */
+
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		_putchar(a[i]);
+	_putchar('\n');
+}
