f81d0a1e9cab550f0123c251dcced12069df2bf4
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 08:24:44 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,33 @@
 
 int _atoi(char *s)
 {
-	int res, i, neg;
+	int res, i, neg, start;
 
 	res = 0;
 	neg = 0;
+	start = 5;
 	for (i = 0; s[i] != '\0'; ++i)
 	{
+		if (start == 0)
+			break;
+
 		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (start == 5)
+			{
+				start = 1
+			}
 			res = res * 10 + s[i] - '0';
+		}
 		else if (s[i] == '-')
 			neg += 1;
+		else if (start = 1)
+			start = 0;
 	}
 
 	if (neg % 2 == 1)
 		res *= -1;
 
 
-	return res;
+	return (res);
 }
