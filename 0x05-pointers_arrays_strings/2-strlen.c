9f747941da99342829c8e9cc379223036afb5328
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-11 04:00:35 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '/0')
+	{
+		i++;
+	}
+	return (i);
+}
