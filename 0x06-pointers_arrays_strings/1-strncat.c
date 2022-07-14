33d50286c38aee3c18139535bcd031cf4a4fbbeb
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-14 00:27:14 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: use most n bytes
+ * Retunr: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char* current = dest + strlen(dest);
+
+	while (*src!= '\0' && n--)
+	{
+		*current++ = *src++;
+	}
+	*current = '\0';
+	return (dest);
+}
