f39eaae257ab018f6670d94b062965f91d073bdc
Joseph Bio Akowuah <joseph.akowuah.35@gmail.com>
2022-07-18 14:29:38 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n > 0)
+	{
+		*dest++ = *src++;
+		n--;
+	}
+	return (dest);
+}
